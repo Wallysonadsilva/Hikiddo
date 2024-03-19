516a4f91aff91ef2cde94c2eb5a6106301744254
wallysonads <wallyson-s@hotmail.com>
2024-03-19 22:11:25 +0000

    map added to location
    - google maps API workin

--- hikiddo/ios/Runner/AppDelegate.swift
+++ hikiddo/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+
+    // GOOGLE API KEY
+    GMSServices.provideAPIKey("AIzaSyAdUgw5z1-goOyDmM0SlWgYPrEeevA0Rrk")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
--- hikiddo/lib/screens/location/components/body.dart
+++ hikiddo/lib/screens/location/components/body.dart
@@ -1,17 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:hikiddo/screens/location/components/background.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+class Body extends StatefulWidget {
+  const Body({
+    super.key,
+  });
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(51.509865, -0.118092);
+
+  void _onMapCreated(GoogleMapController controller){
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    //Size size = MediaQuery.of(context).size;
-    return const Background(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-        ]
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
       ),
     );
   }
